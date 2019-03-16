18442dd6ffa7e51cf56eeb19ea2063803452ea14
samchamper <samchamper@gmail.com>
2019-03-15 21:33:06 -0700

    Change to argparser, update readme.

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 A simple python matplotlib script for visualizing and saving the output of a SLiM spatial simulation.
 
-SLiM already has a visualization/GUI for use on Mac computers. However, for visualizing SLiM runs in a linux environment, when doing
-remote computation, or even just when it is desirable to export a visualization to a video, this tool may be useful.
+SLiM already has a visualization/GUI for use on Mac computers. However, for visualizing SLiM runs in a
+linux environment, when doing remote computation, or even just when it is desirable to export a
+visualization to a video, this tool may be useful.
 
 This repository has a demo showing the kind of videos that this program creates.
 See: https://raw.githubusercontent.com/samchamper/slim_vis/master/pop_supression_example.mp4
@@ -14,29 +15,41 @@ Sam Champer
 
 ## Requirements:
 * python3 (https://www.python.org/downloads/; make sure to "add python path" when you install it)
-* ffmpeg (ffmpeg.org; you will need to add ffmpeg to your path if you are on windows. See: https://www.wikihow.com/Install-FFmpeg-on-Windows)
+* ffmpeg (ffmpeg.org; you will need to add ffmpeg to your path if you are on windows.
+    See: https://www.wikihow.com/Install-FFmpeg-on-Windows)
 * pipenv (after installing python, run ``pip install pipenv``)
 
 ## Installation and Usage:
-1. First, generate a file that this program can read. See: https://github.com/samchamper/slim_vis/blob/master/generating_slim_output.md.
+1. First, generate a file that this program can read.
+    See: https://github.com/samchamper/slim_vis/blob/master/generating_slim_output.md.
 
 1. Place the generated movie file in the directory with this program.
 
 1. Run the following command to install python dependencies:
-```pipenv install```
+```
+pipenv install
+```
 Then you can run the program via:
-```pipenv run vis```
-You can add an optional command line argument if the movie data file doesn't have the default filename "slim_movie":
-```pipenv run vis name_of_movie_data_file.txt```
+```
+pipenv run vis
+```
+* Optional command line arguments (don't actually include the "<>" brackets):
+1. ``-src <filename>`` to specify the source. Default ``slim_movie``
+1. ``-fps <number>`` to specify the fps of the animation. Each frame is a generation, 
+    so this is also generations per second. Default 8.
+1. ``-s`` individual size. Increases for very small populations, decrease for large ones. Default 3.
+1. ``-dim`` The dimensions of the output. Default is 1000, resulting in a 1000*1000 animation.
 
-* Alternatively, you may want to use make to install and run. Use ``make install`` and then ``make run``.
+* For example:
+```
+pipenv run vis -src data_file.txt -fps 8 -s 4 -dim 1080
+```
 
 ## Notes:
 Adding the code to slim to export data to a file that can be read by this program greatly increases SLiM's
 runtime - by as much as a factor of 2. File I/O is slow.
 
 Warning: It can take quite a long time to generate a movie for slim runs with many hundreds/thousands of generations!
-For very long runs, the fps parameter can be tweaked in the slim_vis.py file.
 
 The data files that the SLiM code outputs can be somewhat large: a SLiM run with 10k population and 100
 generations can be close to 20 megabytes.
--- slim_vis.py
+++ slim_vis.py
@@ -3,34 +3,32 @@ Author: Sam Champer
 
 Simple python matplotlib script for visualizing and saving the output of a SLiM spacial simulation.
 
-Note:   This program by default looks for a file called slim_movie.txt, but this can be overridden
+Note:   This program by default looks for a file called 'slim_movie', but this can be overridden
         by specifying the filename by adding it to the command line when the program is run.
+        For a list of command line arguments, run the program followed by -h
 
 Usage: To use this program, code needs to be added to SLiM to output a file that this program can read.
 
 See the github: https://github.com/samchamper/slim_vis
-
 """
 
 import sys
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
-
-# CONFIGURATION GLOBALS - CHANGE THESE TO CHANGE PROGRAM BEHAVIOR:
-FPS = 10
-INDIVIDUAL_SIZE = 3
+from argparse import ArgumentParser
 
 
 def animate(i, subplot, xs, ys, cs):
     """
     Update the animation each frame with the data for that generation.
     """
+    print("   Drawing generation {}/{} \r".format(i, len(xs)), end="", flush=True)
     subplot.clear()
     subplot.set_title("Gen {}".format(i))
     subplot.set_ylim(0, 1)
     subplot.set_xlim(0, 1)
-    subplot.scatter(xs[i], ys[i], color=cs[i], s=INDIVIDUAL_SIZE)
+    subplot.scatter(xs[i], ys[i], color=cs[i], s=individual_size)
 
 
 def main():
@@ -40,17 +38,11 @@ def main():
     2. Parse data into lists for x coords, y coords, and colors.
     3. Make matplotlib plot with an animation function that uses the data.
     4. Export the plot as an mp4.
-    5. Display the plot in a window.
     """
-    # Default filenae.
-    filename = "slim_movie"
-    if len(sys.argv) > 1:
-        # Get file name from command line.
-        filename = sys.argv[1]
-
     # Read and parse data from the file into a list of
     # x coords, y coords, and colors for each generation:
-    with open(filename, 'r') as f:
+    print("Reading {}...".format(source))
+    with open(source, 'r') as f:
         data = f.read()
     data = data[1:].split('G\n')
     for gen in range(len(data)):
@@ -66,17 +58,32 @@ def main():
                 ys[gen].append(int(split[1], 16) / 4095)
                 cs[gen].append([int(split[2], 16) / 255, int(split[3], 16) / 255, int(split[4], 16) / 255])
 
-    # Create and configure a matplotlib figure,
-    # then call the animation function using the data:
+    print("Generating animation...")
+    # Create and configure a matplotlib figure, then call the animation function using the data:
     style.use('dark_background')
-    fig = plt.figure(figsize=(6, 6))
+    fig = plt.figure(figsize=(dimensions/100, dimensions/100))
     subplot = fig.add_subplot(1, 1, 1)
-    anim = animation.FuncAnimation(fig, animate, frames=len(data), interval=1000/FPS, fargs=(subplot, xs, ys, cs))
+    anim = animation.FuncAnimation(fig, animate, frames=len(data), interval=1000/fps, fargs=(subplot, xs, ys, cs))
 
     # Save the animation, and then display it in a window.
-    anim.save('{}.mp4'.format(filename), writer=animation.writers['ffmpeg'](fps=FPS))
-    plt.show()
+    anim.save('{}.mp4'.format(source), writer=animation.writers['ffmpeg'](fps=fps))
+    # plt.show()  # Not particularly useful to show the plot after outputting the movie, but might have a purpose?
+    print("Animation written to {}.mp4.".format(source))
 
 
 if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('-src', '--source', default="slim_movie", type=str,
+                        help='File containing positional data. Default slim_movie')
+    parser.add_argument('-fps', '--fps', default=8, type=int,
+                        help='Frames (generations) per sec for the animation. Default 8.')
+    parser.add_argument('-s', '--individual_size', default=3, type=int,
+                        help='Dot size of individuals. Lower this for high pops, or things will be messy. Default 3.')
+    parser.add_argument('-dim', '--dimensions', default=1000, type=int,
+                        help='Dimensions of the animation. Default 1000 (1000*1000 animation).')
+    args = parser.parse_args()
+    fps = args.fps
+    dimensions = args.dimensions
+    individual_size = args.individual_size
+    source = args.source
     main()
